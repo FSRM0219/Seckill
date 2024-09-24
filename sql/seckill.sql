2452de8995b7d6a936ca16c471ea3a65165f13c5
XPRJ0219 <fsrm0219@qq.com>
2024-09-24 09:27:47 +0800

    2024.9.24

--- sql/seckill.sql
+++ sql/seckill.sql
@@ -1,14 +1,6 @@
-/*
-Source Server         : localhost
-Source Host           : localhost:3306
-Source Database       : seckill
-*/
-
+# Source Database: seckill
 SET FOREIGN_KEY_CHECKS = 0;
 
--- ----------------------------
--- Table structure for sk_goods
--- ----------------------------
 DROP TABLE IF EXISTS `sk_goods`;
 CREATE TABLE `sk_goods`
 (
@@ -24,9 +16,6 @@ CREATE TABLE `sk_goods`
   AUTO_INCREMENT = 3
   DEFAULT CHARSET = utf8;
 
--- ----------------------------
--- Records of sk_goods
--- ----------------------------
 INSERT INTO `sk_goods`
 VALUES ('1', 'iphoneX', 'Apple/苹果iPhone X 全网通4G手机苹果X 10', '/img/iphonex.png',
         'Apple/苹果iPhone X 全网通4G手机苹果X 10', '7788.00', '100');
@@ -34,9 +23,6 @@ INSERT INTO `sk_goods`
 VALUES ('2', '华为 Mate 10', 'Huawei/华为 Mate 10 6G+128G 全网通4G智能手机', '/img/meta10.png',
         'Huawei/华为 Mate 10 6G+128G 全网通4G智能手机', '4199.00', '50');
 
--- ----------------------------
--- Table structure for sk_goods_seckill
--- ----------------------------
 DROP TABLE IF EXISTS `sk_goods_seckill`;
 CREATE TABLE `sk_goods_seckill`
 (
@@ -52,17 +38,11 @@ CREATE TABLE `sk_goods_seckill`
   AUTO_INCREMENT = 3
   DEFAULT CHARSET = utf8;
 
--- ----------------------------
--- Records of sk_goods_seckill
--- ----------------------------
 INSERT INTO `sk_goods_seckill`
 VALUES ('1', '1', '0.01', '8', '2018-05-22 17:22:52', '2018-05-22 18:23:00', '0');
 INSERT INTO `sk_goods_seckill`
 VALUES ('2', '2', '0.01', '8', '2018-04-29 22:56:10', '2018-05-01 22:56:15', '0');
 
--- ----------------------------
--- Table structure for sk_order
--- ----------------------------
 DROP TABLE IF EXISTS `sk_order`;
 CREATE TABLE `sk_order`
 (
@@ -76,15 +56,9 @@ CREATE TABLE `sk_order`
   AUTO_INCREMENT = 11
   DEFAULT CHARSET = utf8;
 
--- ----------------------------
--- Records of sk_order
--- ----------------------------
 INSERT INTO `sk_order`
 VALUES ('10', '18718185897', '1', '1');
 
--- ----------------------------
--- Table structure for sk_order_info
--- ----------------------------
 DROP TABLE IF EXISTS `sk_order_info`;
 CREATE TABLE `sk_order_info`
 (
@@ -104,15 +78,9 @@ CREATE TABLE `sk_order_info`
   AUTO_INCREMENT = 11
   DEFAULT CHARSET = utf8;
 
--- ----------------------------
--- Records of sk_order_info
--- ----------------------------
 INSERT INTO `sk_order_info`
 VALUES ('10', '18718185897', '1', null, 'iphoneX', '1', '7788.00', '1', '0', '2018-05-29 17:02:00', null);
 
--- ----------------------------
--- Table structure for sk_user
--- ----------------------------
 DROP TABLE IF EXISTS `sk_user`;
 CREATE TABLE `sk_user`
 (
@@ -128,9 +96,6 @@ CREATE TABLE `sk_user`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
--- ----------------------------
--- Records of sk_user
--- ----------------------------
 INSERT INTO `sk_user`
 VALUES ('18181818181', 'jesper', 'b7797cce01b4b131b433b6acf4add449', '1a2b3c4d', null, '2018-05-21 21:10:21',
         '2018-05-21 21:10:25', '1');
